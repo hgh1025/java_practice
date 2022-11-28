63ba53ce822e4b974c7db9ebaaf2730770c73f39
hgh1025 <gjwlgnsdk@gmail.com>
2022-11-28 20:40:50 +0900

    first java_practice

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!"); // println = 출력 후 줄바꿈 함.
+        System.out.print("Hello World"); // print() - 출력 후에 줄바꿈 안함
+        System.out.println(3+5);
+        System.out.println("3+5");
+        int i = 10;
+        while (i-- > 0) {
+        System.out.println(i);
+        }
+    }
+}
